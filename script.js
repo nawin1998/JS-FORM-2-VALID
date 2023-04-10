a062357526c3af8fb54731f297f764f6e23b2da6
nawin1998 <120089267+nawin1998@users.noreply.github.com>
2023-04-10 11:36:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let myInput = document.getElementById("password");
+let letter = document.getElementById("letter");
+let capital = document.getElementById("capital");
+let number = document.getElementById("number");
+let length = document.getElementById("length");
+let specialcharacter = document.getElementById("validatecharacters");
+
+ function validatemyInput() {
+  // Validate lowercase letters
+  let lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  let upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  let numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+  //validate sp-character
+  let validatecharacter = /[^A-Za-z0-9-'']/i;
+    if (myInput.value.match(validatecharacter)){
+    specialcharacter.classList.remove("invalid");
+    specialcharacter.classList.add("valid");
+  } 
+  else{
+    specialcharacter.classList.remove("valid");
+    specialcharacter.classList.add("invalid");
+  }
+}
+
